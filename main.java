c56da92affaa9240939f984c17759b438a48ecb2
PhongHa <phongha8074@gmail.com>
2025-06-02 23:46:14 +0700

    modified:   main.java

--- main.java
+++ main.java
@@ -1,29 +1,28 @@
-class Main 
-{
-    private class MyClass 
-    {
+class Main {
+    private static class MyClass { // Made MyClass static
+
         // Method to print a message
-        public void printMessage() 
-        {
+        public void printMessage() {
             System.out.println("Hello, World!");
         }
 
         // Method to get the current date as a string
-        public String getCurrentDate() 
-        {
+        public String getCurrentDate() {
             return java.time.LocalDate.now().toString();
         }
     }
 
     // Main method to run the program
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         // Create a new instance of the class
         MyClass myObject = new MyClass();
+
         // Call the method to print the message
         myObject.printMessage();
+
         // Call the method to get the current date
         String currentDate = myObject.getCurrentDate();
+
         // Print the current date
         System.out.println("Current date: " + currentDate);
     }
