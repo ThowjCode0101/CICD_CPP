8360e1f7eabc653c1a9a2846fe2ff1ce533b6c11
PhongHa <phongha8074@gmail.com>
2025-06-05 00:10:21 +0700

    cleann all

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/WORK/CODE/CICD/CICD_CPP/tests
-# Build directory: C:/WORK/CODE/CICD/CICD_CPP/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(HelloTest "C:/WORK/CODE/CICD/CICD_CPP/build/tests/hello_test.exe")
-set_tests_properties(HelloTest PROPERTIES  WORKING_DIRECTORY "C:/WORK/CODE/CICD/CICD_CPP/build/tests" _BACKTRACE_TRIPLES "C:/WORK/CODE/CICD/CICD_CPP/tests/CMakeLists.txt;17;add_test;C:/WORK/CODE/CICD/CICD_CPP/tests/CMakeLists.txt;0;")
